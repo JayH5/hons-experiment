3e15f89e8cf9be87d8f861bfee3fad937f54f467
Jamie Hewland <jhewland@gmail.com>
2014-09-15 16:47:55 +0200

    Add GUI demo to ScoreCalculator

--- src/main/java/za/redbridge/experiment/ScoreCalculator.java
+++ src/main/java/za/redbridge/experiment/ScoreCalculator.java
@@ -6,8 +6,10 @@ import org.encog.ml.MLMethod;
 import java.awt.Color;
 import java.awt.Paint;
 
+import sim.display.Console;
 import za.redbridge.experiment.MMNEAT.MMNEATNetwork;
 import za.redbridge.simulator.Simulation;
+import za.redbridge.simulator.SimulationGUI;
 import za.redbridge.simulator.config.SimConfig;
 import za.redbridge.simulator.factories.HomogeneousRobotFactory;
 import za.redbridge.simulator.factories.RobotFactory;
@@ -44,6 +46,21 @@ public class ScoreCalculator implements CalculateScore {
         // Get the fitness
         lastScore = simulation.getFitness();
         return lastScore;
+    }
+
+    public void demo(MMNEATNetwork network) {
+        // Create the robot and resource factories
+        RobotFactory robotFactory = new HomogeneousRobotFactory(new MMNEATPhenotype(network),
+                DEFAULT_ROBOT_MASS, DEFAULT_ROBOT_RADIUS, DEFAULT_ROBOT_PAINT);
+
+        // Create the simulation and run it
+        Simulation simulation = new Simulation(simConfig, robotFactory, 15);
+
+        SimulationGUI video = new SimulationGUI(simulation);
+
+        //new console which displays this simulation
+        Console console = new Console(video);
+        console.setVisible(true);
     }
 
     @Override
