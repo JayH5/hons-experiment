ad1bce3ec0a4a404375874bc4292dd6129d7ee07
Jamie Hewland <jhewland@gmail.com>
2014-09-18 17:08:26 +0200

    Update for simulator changes

--- src/main/java/za/redbridge/experiment/ScoreCalculator.java
+++ src/main/java/za/redbridge/experiment/ScoreCalculator.java
@@ -3,9 +3,6 @@ package za.redbridge.experiment;
 import org.encog.ml.CalculateScore;
 import org.encog.ml.MLMethod;
 
-import java.awt.Color;
-import java.awt.Paint;
-
 import sim.display.Console;
 import za.redbridge.experiment.MMNEAT.MMNEATNetwork;
 import za.redbridge.simulator.Simulation;
@@ -18,10 +15,6 @@ import za.redbridge.simulator.factories.RobotFactory;
  * Created by jamie on 2014/09/09.
  */
 public class ScoreCalculator implements CalculateScore {
-
-    private static final double DEFAULT_ROBOT_MASS = 0.7;
-    private static final double DEFAULT_ROBOT_RADIUS = 0.15;
-    private static final Paint DEFAULT_ROBOT_PAINT = Color.BLACK;
 
     private final SimConfig simConfig;
 
@@ -38,10 +31,11 @@ public class ScoreCalculator implements CalculateScore {
 
         // Create the robot and resource factories
         RobotFactory robotFactory = new HomogeneousRobotFactory(new MMNEATPhenotype(network),
-                DEFAULT_ROBOT_MASS, DEFAULT_ROBOT_RADIUS, DEFAULT_ROBOT_PAINT);
+                simConfig.getRobotMass(), simConfig.getRobotRadius(), simConfig.getRobotColour(),
+                simConfig.getObjectsRobots());
 
         // Create the simulation and run it
-        Simulation simulation = new Simulation(simConfig, robotFactory, 15);
+        Simulation simulation = new Simulation(simConfig, robotFactory);
         simulation.run();
 
         // Get the fitness and update the total score
@@ -54,10 +48,11 @@ public class ScoreCalculator implements CalculateScore {
     public void demo(MMNEATNetwork network) {
         // Create the robot and resource factories
         RobotFactory robotFactory = new HomogeneousRobotFactory(new MMNEATPhenotype(network),
-                DEFAULT_ROBOT_MASS, DEFAULT_ROBOT_RADIUS, DEFAULT_ROBOT_PAINT);
+                simConfig.getRobotMass(), simConfig.getRobotRadius(), simConfig.getRobotColour(),
+                simConfig.getObjectsRobots());
 
         // Create the simulation and run it
-        Simulation simulation = new Simulation(simConfig, robotFactory, 15);
+        Simulation simulation = new Simulation(simConfig, robotFactory);
 
         SimulationGUI video = new SimulationGUI(simulation);
 
